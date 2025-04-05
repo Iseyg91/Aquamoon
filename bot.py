7707bc6ae90173d56e55cae27144c3ac50166ae4
Iseyg91 <gokalpahmet74@gmail.com>
2025-04-05 15:42:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ token = os.environ['AQUAMOON']
 intents = discord.Intents.all()
 start_time = time.time()
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix="+", intents=intents)
+bot = commands.Bot(command_prefix=".", intents=intents)
 
 @bot.event
 async def on_ready():
